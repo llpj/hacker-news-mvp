af1511d2730f98b5d2efea2407156cb0229b3352
Emma Guy <emmag87@gmail.com>
2015-03-28 11:57:14 +0000

    added some tests to the comments presenter

--- mvp/src/test/java/com/emmaguy/hn/presenter/CommentsPresenterImplTest.java
+++ mvp/src/test/java/com/emmaguy/hn/presenter/CommentsPresenterImplTest.java
@@ -1,5 +1,6 @@
 package com.emmaguy.hn.presenter;
 
+import com.emmaguy.hn.model.Comment;
 import com.emmaguy.hn.model.data.HackerNewsDataSource;
 import com.emmaguy.hn.presenter.comments.CommentsPresenterImpl;
 import com.emmaguy.hn.view.CommentsView;
@@ -8,7 +9,6 @@ import com.squareup.otto.Bus;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.ArrayList;
@@ -55,5 +55,36 @@ public class CommentsPresenterImplTest {
 
         verify(mMockNetworkBus, times(1)).unregister(mPresenter);
         verifyNoMoreInteractions(mMockNetworkBus);
+    }
+
+    @Test
+    public void test_onStart_showsLoadingIndicator() {
+        mPresenter.onStart();
+
+        verify(mMockView, times(1)).showLoadingIndicator();
+        verifyNoMoreInteractions(mMockView);
+    }
+
+    @Test
+    public void test_onStart_retrievedComments() {
+        mPresenter.onStart();
+
+        verify(mMockDataSource, times(1)).getComments(mIds);
+        verifyNoMoreInteractions(mMockDataSource);
+    }
+
+    @Test
+    public void test_onCommentsReceived_hidesLoadingIndicator() {
+        mPresenter.onCommentsReceived(new ArrayList<Comment>());
+
+        verify(mMockView, times(1)).hideLoadingIndicator();
+    }
+
+    @Test
+    public void test_onCommentsReceived_showsComments() {
+        ArrayList<Comment> comments = new ArrayList<>();
+        mPresenter.onCommentsReceived(comments);
+
+        verify(mMockView, times(1)).showComments(comments);
     }
 }
